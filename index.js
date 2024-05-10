2714250ff7401beebb2c797404f24223e3fbc23f
divvsaxena <saxenadivv@gmail.com>
2024-05-10 17:41:24 +0530

    Add assets

---
+++ index.js
@@ -0,0 +1,64 @@
+//UI ELEMENTS
+
+const homeEl = document.getElementById('home')
+const aboutMeEl = document.getElementById('aboutMe')
+const servicesEl = document.getElementById('services')
+const locationEl = document.getElementById('location')
+
+let click = true
+
+document.getElementById('hamburger').addEventListener('click', (e) => {
+    if(click){
+        renderHamburger()
+        click = false
+    }else {
+        notRenderHamburger()
+        click = true
+    }
+    
+})
+
+
+function renderHamburger(){
+    document.getElementById('list').style.display = `flex`
+}
+
+function notRenderHamburger(){
+    document.getElementById('list').style.display = `none`
+}
+
+function scrollFunctionOne(){
+    homeEl.scrollIntoView({
+        block:'start',
+        behavior:'smooth',
+        inline:'start'
+    })
+    notRenderHamburger()
+}
+
+function scrollFunctionTwo(){
+    aboutMeEl.scrollIntoView({
+        block:'start',
+        behavior:'smooth',
+        inline:'start'
+    })
+    notRenderHamburger()
+}
+
+function scrollFunctionThree(){
+    servicesEl.scrollIntoView({
+        block:'start',
+        behavior:'smooth',
+        inline:'start'
+    })
+    notRenderHamburger()
+}
+
+function scrollFunctionFour(){
+    locationEl.scrollIntoView({
+        block:'start',
+        behavior:'smooth',
+        inline:'start'
+    })
+    notRenderHamburger()
+}
